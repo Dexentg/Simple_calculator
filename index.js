45b4751ce9137d41a086f62bf3d3b80d182215ba
Dexentg <160043330+Dexentg@users.noreply.github.com>
2024-03-19 01:24:55 +0500

    Add files via upload
    A simple command line calculator which is made by typescript & inquirer.

---
+++ index.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter your first number", type: "number", name: "FirstNumber" },
+    { message: "enter your second number", type: "number", name: "SecondNumber" },
+    {
+        message: "select your operaotr",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "division", "multiplication"],
+    },
+]);
+if (answer.operator === "addition") {
+    console.log("your value is", answer.FirstNumber + answer.SecondNumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log("your value is", answer.FirstNumber - answer.SecondNumber);
+}
+else if (answer.operator === "division") {
+    console.log("ypur value is", answer.FirstNumber / answer.SecondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log("ypur value is", answer.FirstNumber * answer.SecondNumber);
+}
